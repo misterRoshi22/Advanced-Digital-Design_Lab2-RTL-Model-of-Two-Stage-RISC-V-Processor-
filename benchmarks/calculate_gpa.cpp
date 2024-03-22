cf9b880406e516343f7122acdb88627d0eddac79
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2024-03-22 16:52:42 +0300

    Update calculate_gpa.cpp

--- benchmarks/calculate_gpa.cpp
+++ benchmarks/calculate_gpa.cpp
@@ -5,7 +5,7 @@ int main(void) {
     int courses[5] = {99, 94, 86, 84, 59};
     int lab = 89;
     int sum = 0;
-    int n = 0;
+    int n = 5;
 
     for(int i = 0; i < n; i++) {
         sum += courses[i];
@@ -14,4 +14,5 @@ int main(void) {
     sum += lab;
 
     int avg = sum/16;
+    cout << avg << endl;
 }
