9c2d5428c74ab8d9cbe4b632a9d5b4f82bbdd708
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2024-03-23 01:10:25 +0300

    Update register_file.v

--- modules/register_file.v
+++ modules/register_file.v
@@ -1,14 +1,14 @@
 module register_file(Clock, Reset_n, Read_addr1, Read_addr2, Write_addr, Write_En, Write_data, Read_data1, Read_data2);
-	input  			Clock, Reset_n;	// Clock and Reset
+	input  		Clock, Reset_n;			// Clock and Reset
 	input  [4:0] 	Read_addr1;			// Address of register 1
 	input  [4:0] 	Read_addr2;			// Address of register 2
 	input  [4:0] 	Write_addr;			// Used in load operation, (write_address)
-	input  			Write_En;			// This will only be 1 in load operation else its zero
+	input  		Write_En;			// This will only be 1 in load operation else its zero
 	input  [31:0] 	Write_data;			// Used in load operation, (write_data = &Op2 from RAM)
 	output [31:0] 	Read_data1;			// Value stored in register 1
 	output [31:0] 	Read_data2;			// Value stored in register 2
 
-	reg 	[31:0] 	registers [0:31];	// The 32 registers comprising the register file
+	reg    [31:0] 	registers [0:31];	// The 32 registers comprising the register file
 	
 	integer i;
 
