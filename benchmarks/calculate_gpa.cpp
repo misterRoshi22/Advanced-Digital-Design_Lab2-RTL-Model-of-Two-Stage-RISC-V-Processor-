f05e140804cfbced55cfea84f24673ac856f8d84
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2024-03-22 16:35:35 +0300

    Create calculate_gpa.cpp

---
+++ benchmarks/calculate_gpa.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int courses[5] = {99, 94, 86, 84, 59};
+    int lab = 89;
+
+    int sum = 0;
+    for(int i = 0; i < 5; i++) {
+        sum += courses[i];
+    }
+    sum *= 3;
+    sum += lab;
+
+    int avg = sum/16;
+}
