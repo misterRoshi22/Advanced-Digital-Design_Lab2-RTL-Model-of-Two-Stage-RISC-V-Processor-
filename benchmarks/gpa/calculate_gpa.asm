ef2f447d3b2c7f86c0aeaa88a6b5e65028a9e329
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2024-03-22 17:25:09 +0300

    Create calculate_gpa.asm

---
+++ benchmarks/gpa/calculate_gpa.asm
@@ -0,0 +1,18 @@
+addi s1 x0 89
+add s0 x0 x0
+addi t0 x0 5
+addi t1 x0 0
+beq t1 t0 10
+slli t2 t1 2
+lw t3 0 t2
+add s0 s0 t3
+addi t1 t1 1
+beq x0 x0 -12
+add s4 x0 s0
+add s0 s0 s0
+add s0 s0 s4
+add s0 s0 s1
+srli s2 s0 4
+
+
+
