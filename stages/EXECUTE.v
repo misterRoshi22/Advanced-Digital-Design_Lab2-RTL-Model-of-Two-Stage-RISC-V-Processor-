2dcda74a533fa7e0e581f5f2cbf955b22f80d787
Mahmoud Abu-Qtiesh <109997866+misterRoshi22@users.noreply.github.com>
2024-03-23 11:45:40 +0300

    Update EXECUTE.v

--- stages/EXECUTE.v
+++ stages/EXECUTE.v
@@ -3,72 +3,72 @@ module EXECUTE(clk, reset_n, instruction, pc, pc_plus_4,
  jump_address, jalr_address, branch, jump, jalr, flush);
 
 	//Universal Inputs
-	input 			clk, reset_n;
+	input 		clk, reset_n;
 	
 	//Inputs From REG
-	input [31:0]	instruction;
-	input [10:0] 	pc, pc_plus_4;
-	input [6:0]  	opcode;
-	input [4:0]		rs1, rs2, rd;
-	input [2:0]		funct3;
-	input 			add_sub;
+	input	[31:0]	instruction;
+	input	[10:0] 	pc, pc_plus_4;
+	input	[6:0]  	opcode;
+	input	[4:0]	rs1, rs2, rd;
+	input 	[2:0]	funct3;
+	input 		add_sub;
 	
 	//Outputs To FETCH
-	output [10:0] 	branch_address, jump_address, jalr_address;
-	output 		  	branch, jump, jalr;
+	output 	[10:0] 	branch_address, jump_address, jalr_address;
+	output 		branch, jump, jalr;
 	
 	//Outputs To REG
-	output			flush;
+	output		flush;
 	
 	//Reference Voltages
-	wire 				GND, VCC;
+	wire 		GND, VCC;
 	
 	//Outputs from Control Unit
-	wire  [3:0] 	Branch;
-	wire  [1:0]		Jump;
-	wire 				MemRead, MemWrite; 
-	wire 	[3:0]		ALUOp;
-	wire 	[1:0]		MemtoReg; 
-	wire				ALUSrc;
-	wire 				RegWrite;
+	wire  	[3:0] 	Branch;
+	wire  	[1:0]	Jump;
+	wire 		MemRead, MemWrite; 
+	wire 	[3:0]	ALUOp;
+	wire 	[1:0]	MemtoReg; 
+	wire		ALUSrc;
+	wire 		RegWrite;
 	
 	//Outputs from Register File
-	wire [31:0]		Read_data1, Read_data2;
+	wire 	[31:0]	Read_data1, Read_data2;
 	
 	//Outputs from Immediate
-	wire [31:0]		Immediate;
+	wire 	[31:0] Immediate;
 		
 	//Output From ALU MUX
-	wire [31:0] 	ALU_B;
+	wire 	[31:0] 	ALU_B;
 	
 	//Outputs from ALU
-	wire [31:0]		ALU_Result;
-	wire				Zero;
+	wire 	[31:0]	ALU_Result;
+	wire		Zero;
 	
 	//Outputs from MemToReg MUX
-	wire [31:0]		write_data_reg_file;
-	wire [31:0] 	jal_ra;
+	wire 	[31:0]	write_data_reg_file;
+	wire 	[31:0] 	jal_ra;
 	
 	//Outputs from Data Memory
-	wire [31:0] 	read_data_memory;
+	wire 	[31:0] 	read_data_memory;
 	
 	//Inputs to Register File
-	wire [4:0]		WriteReg;
-	wire [4:0]		ra;
+	wire 	[4:0]	WriteReg;
+	wire 	[4:0]	ra;
 	
 	//Inputs to Jalr Adder
-	wire [10:0] 	JALR_A, JALR_B;
+	wire 	[10:0] 	JALR_A, JALR_B;
 	
 	//Inputs to Branch Adder
-	wire [10:0] 	BRANCH_A, BRANCH_B;
+	wire 	[10:0] 	BRANCH_A, BRANCH_B;
 		
 	//Inputs to Data Memory
-	wire [10:0] 	address;
-	wire [31:0]		write_data_memory;
+	wire 	[10:0] 	address;
+	wire 	[31:0]	write_data_memory;
 	
 	//Determines Control Signals For Rest of Modules
 	control_unit control_unit(opcode, funct3, add_sub, Branch, Jump, MemRead,
-   MemWrite, ALUOp, MemtoReg, ALUSrc, RegWrite);
+   	MemWrite, ALUOp, MemtoReg, ALUSrc, RegWrite);
 	
 	//32x32 bit Register File used to store recent values
 	register_file register_file(clk, reset_n, rs1, rs2,
